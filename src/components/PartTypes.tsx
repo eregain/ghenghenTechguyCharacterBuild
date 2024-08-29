bbbbb99dac0d62c27b17d66eda3e33bc1cb6bf54
eregain <talktostevenson@gmail.com>
2024-08-29 20:29:40 +0100

    ghen ghen charcyter builder for tech and regular  guy
    hvffhgvkgbug

---
+++ src/components/PartTypes.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { ConfigPartType } from "../interfaces/Config";
+
+import classes from "./PartTypes.module.scss";
+
+interface PartTypesProps {
+  partTypes: ConfigPartType[];
+  setPartType: (partTypeId: number) => void;
+  partType: number | undefined;
+  save: () => void;
+  share: () => void;
+  refresh: () => void;
+}
+
+export const PartTypes = (props: PartTypesProps) => {
+  const urlPrefix = process.env.PUBLIC_URL + "/parts_icons";
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.actions}>
+        <div
+          className={classes.action}
+          onClick={() => {
+            props.refresh();
+          }}
+        >
+          <img src={urlPrefix + "/refresh.png"} alt="refresh" />
+        </div>
+        <div
+          className={classes.action}
+          onClick={e => {
+            props.save();
+          }}
+        >
+          <img src={urlPrefix + "/save.png"} alt="save" />
+        </div>
+        <div
+          className={classes.action}
+          onClick={() => {
+            props.share();
+          }}
+        >
+          <img src={urlPrefix + "/share.png"} alt="share" />
+        </div>
+      </div>
+
+      <div className={classes.partTypes}>
+        {props.partTypes.map((partType, index) => (
+          <div
+            key={index}
+            className={
+              partType.id === props.partType
+                ? classes.partTypeSelected
+                : classes.partType
+            }
+            onClick={() => {
+              props.setPartType(partType.id);
+            }}
+          >
+            <img
+              src={urlPrefix + "/" + partType.iconFilename + ".png"}
+              alt={partType.iconFilename}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
